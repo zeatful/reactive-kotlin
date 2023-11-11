8361a7d45e1e69d95c627f7b3c16bb33595615f4
zeatful <zeatful@github.com>
2023-11-10 23:41:25 -0500

    clean up build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,15 +20,12 @@ repositories {
 }
 
 dependencies {
-//	implementation("com.google.cloud:spring-cloud-gcp-starter-sql-postgres-r2dbc:4.8.3")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-//	runtimeOnly("org.postgresql:postgresql")
-//	runtimeOnly("org.postgresql:r2dbc-postgresql")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("io.r2dbc:r2dbc-h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
